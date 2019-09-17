19b401baeb45a774bc7e9927737873e962ac834e
leechang0423 <leechang0423@naver.com>
2019-09-17 13:11:57 +0900

    feature #project and domain create

---
+++ src/test/java/me/changjun/demorestapi/events/EventTest.java
@@ -0,0 +1,34 @@
+package me.changjun.demorestapi.events;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class EventTest {
+
+    @Test
+    public void builder() {
+        Event event = Event.builder()
+                .name("Iflearn Spring Rest Api")
+                .description("REST API development with Spring")
+                .build();
+        assertThat(event).isNotNull();
+    }
+
+    @Test
+    public void javaBean() {
+        //given
+        String name = "Event";
+        String description = "Spring";
+
+        //when
+        Event event = new Event();
+        event.setName(name);
+        event.setDescription(description);
+
+        //then
+        assertThat(event.getName()).isEqualTo(name);
+        assertThat(event.getDescription()).isEqualTo(description);
+
+    }
+}
