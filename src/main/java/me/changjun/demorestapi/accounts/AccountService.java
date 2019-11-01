2539e2d25008fc6693811a8d05f36f6f178e0398
leechang0423 <leechang0423@naver.com>
2019-11-01 14:17:19 +0900

    #feature "AccountService userDetails return AccountAdapter"

--- src/main/java/me/changjun/demorestapi/accounts/AccountService.java
+++ src/main/java/me/changjun/demorestapi/accounts/AccountService.java
@@ -1,47 +1,46 @@
 package me.changjun.demorestapi.accounts;
 
-import java.util.Collection;
-import java.util.Set;
-import java.util.stream.Collectors;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 @Service
 public class AccountService implements UserDetailsService {
 
-  private final AccountRepository accountRepository;
-  private final PasswordEncoder passwordEncoder;
+    private final AccountRepository accountRepository;
+    private final PasswordEncoder passwordEncoder;
 
-  public AccountService(AccountRepository accountRepository,
-      PasswordEncoder passwordEncoder) {
-    this.accountRepository = accountRepository;
-    this.passwordEncoder = passwordEncoder;
-  }
+    public AccountService(AccountRepository accountRepository,
+                          PasswordEncoder passwordEncoder) {
+        this.accountRepository = accountRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
 
-  public Account saveAccount(Account account) {
-    account.setPassword(this.passwordEncoder.encode(account.getPassword()));
-    return this.accountRepository.save(account);
-  }
+    public Account saveAccount(Account account) {
+        account.setPassword(this.passwordEncoder.encode(account.getPassword()));
+        return this.accountRepository.save(account);
+    }
 
-  @Override
-  public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-    Account account = accountRepository.findByEmail(email)
-        .orElseThrow(
-            () -> new UsernameNotFoundException(email)
-        );
-    return new User(account.getEmail(), account.getPassword(),
-        authorities(account.getAccountRoles()));
-  }
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Account account = accountRepository.findByEmail(email)
+                .orElseThrow(
+                        () -> new UsernameNotFoundException(email)
+                );
+        return new AccountAdapter(account);
+    }
 
-  private Collection<? extends GrantedAuthority> authorities(Set<AccountRole> roles) {
-    return roles.stream().map(role ->
-        new SimpleGrantedAuthority("ROLE_" + role.name())
-    ).collect(Collectors.toSet());
-  }
+    private Collection<? extends GrantedAuthority> authorities(Set<AccountRole> roles) {
+        return roles.stream().map(role ->
+                new SimpleGrantedAuthority("ROLE_" + role.name())
+        ).collect(Collectors.toSet());
+    }
 }
